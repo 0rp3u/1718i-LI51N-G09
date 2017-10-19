b015b3948c480a8e225731108922dbd8a89c0561
0rph3u <david_mfra@hotmail.com>
2017-10-19 17:51:11 +0100

    added adapter

--- app/src/main/java/pdm_1718i/yamda/ui/adapters/SimpleMovieAdapter.kt
+++ app/src/main/java/pdm_1718i/yamda/ui/adapters/SimpleMovieAdapter.kt
@@ -1,5 +1,17 @@
 package pdm_1718i.yamda.ui.adapters
 
+import android.app.Activity
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import pdm_1718i.yamda.model.Movie
+
 /**
  * Created by orpheu on 10/19/17.
  */
+class SimplesMovieAdapter(private val context : Activity, private val searchedMovies: List<Movie>,
+                          private val itemClick: (Movie) -> Unit) : ArrayAdapter<Movie>(context, 1, searchedMovies){
+
+
+
+}
