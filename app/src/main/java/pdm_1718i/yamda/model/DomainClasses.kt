70e8b51365d8878faa5c0f81fe73f5bcb8df9836
0rph3u <david_mfra@hotmail.com>
2017-10-27 18:36:22 +0100

    implemented cache so we don't need to put the domain on the bunde domain is not parcelable anymore

--- app/src/main/java/pdm_1718i/yamda/model/DomainClasses.kt
+++ app/src/main/java/pdm_1718i/yamda/model/DomainClasses.kt
@@ -14,40 +14,8 @@ data class Movie(
         val title : String,
         val backdrop_path : String,
         val vote_average: Double
-) : Parcelable{
+)
 
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readInt(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readDouble())
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString( this.poster_path )
-        dest.writeString( this.release_date )
-        dest.writeInt( this.id )
-        dest.writeString( this.title )
-        dest.writeString ( this.backdrop_path )
-        dest.writeDouble( this.vote_average )
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
 
 data class DetailedMovie(
         val poster_path: String?,
@@ -58,39 +26,7 @@ data class DetailedMovie(
         val budget: Int,
         val genres: List<Genre>,
         val overview: String?
-) : Parcelable {
-    constructor(source: Parcel) : this(
-            source.readString(),
-            source.readString(),
-            source.readInt(),
-            source.readString(),
-            source.readFloat(),
-            source.readInt(),
-            ArrayList<Genre>().apply { source.readList(this, Genre::class.java.classLoader) },
-            source.readString()
-    )
-
-    override fun describeContents() = 0
-
-    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
-        writeString(poster_path)
-        writeString(release_date)
-        writeInt(id)
-        writeString(title)
-        writeFloat(vote_average)
-        writeInt(budget)
-        writeList(genres)
-        writeString(overview)
-    }
-
-    companion object {
-        @JvmField
-        val CREATOR: Parcelable.Creator<DetailedMovie> = object : Parcelable.Creator<DetailedMovie> {
-            override fun createFromParcel(source: Parcel): DetailedMovie = DetailedMovie(source)
-            override fun newArray(size: Int): Array<DetailedMovie?> = arrayOfNulls(size)
-        }
-    }
-}
+)
 
 data class Genre(
         val name: String
--- app/src/main/java/pdm_1718i/yamda/ui/activities/BaseActivity.kt
+++ app/src/main/java/pdm_1718i/yamda/ui/activities/BaseActivity.kt
@@ -5,10 +5,10 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.SearchView
 import android.view.Menu
 import android.view.MenuItem
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.SearchView
 import pdm_1718i.yamda.R
 
 
