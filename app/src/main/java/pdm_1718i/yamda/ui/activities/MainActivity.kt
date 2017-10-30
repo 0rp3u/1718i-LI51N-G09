793fa8c65b4ab1e7d8b28c58992af944eadc62c1
edgaralves33017 <33017@alunos.isel.ipl.pt>
2017-10-30 22:57:07 +0000

    MainActivity.kt >Changed main activity to now support RecyclerViews

--- app/src/main/java/pdm_1718i/yamda/ui/activities/MainActivity.kt
+++ app/src/main/java/pdm_1718i/yamda/ui/activities/MainActivity.kt
@@ -2,12 +2,16 @@ package pdm_1718i.yamda.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import pdm_1718i.yamda.R
 import pdm_1718i.yamda.data.server.Options
 import pdm_1718i.yamda.model.Movie
 import pdm_1718i.yamda.ui.App
+import pdm_1718i.yamda.ui.adapters.MainActAdapter
 
 class MainActivity : BaseActivity() {
     private val REQUEST_TYPE = "requestType"
@@ -15,8 +19,8 @@ class MainActivity : BaseActivity() {
     private val DEFAULT_PAGINATION: Int = 1
     private val DEFAULT_ITEM_NUMBER: Int = 4
 
-    private val POPULAR_ITEM_LIST: IntArray =
-            intArrayOf(R.id.popular_image_1, R.id.popular_image_2, R.id.popular_image_3, R.id.popular_image_4)
+    //private val POPULAR_ITEM_LIST: IntArray =
+    //        intArrayOf(R.id.popular_image_1, R.id.popular_image_2, R.id.popular_image_3, R.id.popular_image_4)
     private val UPCOMING_ITEM_LIST: IntArray =
             intArrayOf(R.id.upcoming_image_1, R.id.upcoming_image_2, R.id.upcoming_image_3, R.id.upcoming_image_4)
     private val PLAYING_ITEM_LIST: IntArray =
@@ -26,8 +30,8 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
-
-        App.moviesProvider.popularMovies(DEFAULT_PAGINATION, {
+/*
+        App.moviesProvider.nowPlayingMovies(DEFAULT_PAGINATION, {
             getImageAndSet(it, DEFAULT_ITEM_NUMBER, POPULAR_ITEM_LIST)
         })
 
@@ -35,9 +39,38 @@ class MainActivity : BaseActivity() {
             getImageAndSet(it, DEFAULT_ITEM_NUMBER, UPCOMING_ITEM_LIST)
         })
 
-        App.moviesProvider.nowPlayingMovies(DEFAULT_PAGINATION,{
+        App.moviesProvider.popularMovies(DEFAULT_PAGINATION,{
             getImageAndSet(it, DEFAULT_ITEM_NUMBER, PLAYING_ITEM_LIST)
         })
+*/
+
+        val recyclerViewNowPlaying = findViewById(R.id.recycler_view_nowplaying) as RecyclerView
+        recyclerViewNowPlaying.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
+        App.moviesProvider.nowPlayingMovies(DEFAULT_PAGINATION,{
+            val adapter = MainActAdapter(it)
+            recyclerViewNowPlaying.adapter = adapter
+        })
+
+        val recyclerViewUpcoming = findViewById(R.id.recycler_view_upcoming) as RecyclerView
+        recyclerViewUpcoming.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
+        App.moviesProvider.upcomingMovies(DEFAULT_PAGINATION,{
+            val adapter = MainActAdapter(it)
+            recyclerViewUpcoming.adapter = adapter
+        })
+
+        val recyclerViewPopular = findViewById(R.id.recycler_view_popular) as RecyclerView
+        recyclerViewPopular.layoutManager = LinearLayoutManager(this, LinearLayout.HORIZONTAL, false)
+        App.moviesProvider.popularMovies(DEFAULT_PAGINATION,{
+            val adapter = MainActAdapter(it)
+            recyclerViewPopular.adapter = adapter
+        })
+
+
+
+
+
+
+
     }
 
     fun onPopularMore(view: View){
