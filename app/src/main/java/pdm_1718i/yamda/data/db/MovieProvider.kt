68e1c9531de1eb3844f893a5b75509dbd09ca8e2
0rph3u <david_mfra@hotmail.com>
2017-11-28 18:14:03 +0000

    Early implementation of provider, does not take in consideration the contract default sort order!

--- app/src/main/java/pdm_1718i/yamda/data/db/MovieProvider.kt
+++ app/src/main/java/pdm_1718i/yamda/data/db/MovieProvider.kt
@@ -1,59 +1,55 @@
 package pdm_1718i.yamda.data.db
 
-import android.content.ContentProvider
-import android.content.ContentValues
-import android.content.UriMatcher
+import android.content.*
 import android.database.Cursor
 import android.database.sqlite.SQLiteQueryBuilder
 import android.net.Uri
-import android.text.TextUtils
-
 
 class MovieProvider: ContentProvider(){
 
     object DETAILS{
-        val CURSOR = 1
+        val LIST = 1
         val ITEM = 2
     }
 
     object UPCOMING{
-        val CURSOR = 3
+        val LIST = 3
         val ITEM = 4
     }
 
     object THEATERS{
-        val CURSOR = 5
+        val LIST = 5
         val ITEM = 6
     }
 
-    private val URI_MATCHER: UriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+    private val uriMatcher: UriMatcher = UriMatcher(UriMatcher.NO_MATCH)
 
     init {
 
         /*****************************************************************************/
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 MovieContract.NowPlayingMovies.RESOURCE,
-                THEATERS.CURSOR)
+                THEATERS.LIST)
 
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 "${MovieContract.NowPlayingMovies.RESOURCE}/#",
                 THEATERS.ITEM)
 
         /*****************************************************************************/
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 MovieContract.UpcomingMovies.RESOURCE,
-                UPCOMING.CURSOR)
+                UPCOMING.LIST)
 
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 "${MovieContract.UpcomingMovies.RESOURCE}/#",
                 UPCOMING.ITEM)
 
         /*****************************************************************************/
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 MovieContract.MovieDetails.RESOURCE,
-                DETAILS.CURSOR)
+                DETAILS.LIST)
 
-        URI_MATCHER.addURI(MovieContract.AUTHORITY,
+        uriMatcher.addURI(MovieContract.AUTHORITY,
                 "${MovieContract.MovieDetails.RESOURCE}/#",
                 DETAILS.ITEM)
 
@@ -69,60 +65,114 @@ class MovieProvider: ContentProvider(){
     }
 
     override fun getType(uri: Uri?): String {
-        return when(URI_MATCHER.match(uri))
+        return when(uriMatcher.match(uri))
         {
-            THEATERS.CURSOR -> MovieContract.NowPlayingMovies.CONTENT_TYPE
+            THEATERS.LIST -> MovieContract.NowPlayingMovies.CONTENT_TYPE
             THEATERS.ITEM ->  MovieContract.NowPlayingMovies.CONTENT_ITEM_TYPE
 
-            UPCOMING.CURSOR -> MovieContract.UpcomingMovies.CONTENT_TYPE
+            UPCOMING.LIST -> MovieContract.UpcomingMovies.CONTENT_TYPE
             UPCOMING.ITEM -> MovieContract.UpcomingMovies.CONTENT_ITEM_TYPE
 
-            DETAILS.CURSOR -> MovieContract.MovieDetails.CONTENT_TYPE
+            DETAILS.LIST -> MovieContract.MovieDetails.CONTENT_TYPE
             DETAILS.ITEM -> MovieContract.MovieDetails.CONTENT_ITEM_TYPE
 
             else-> throw badUri(uri)
         }
     }
+    /**
+     * Helper function used to obtain the table name based on the
+     * given [uri]
+     * @param [uri] The table URI
+     * @return A [String] instance bearing the table name
+     * @throws IllegalArgumentException if the received [uri] does not refer to an existing table
+     */
+    private fun resolveTableInfoFromUri(uri: Uri): String = when (uriMatcher.match(uri)) {
+            UPCOMING.LIST -> DbSchema.UpcomingMovies.TBL_NAME
+            THEATERS.LIST -> DbSchema.NowPlayingMovies.TBL_NAME
+            DETAILS.LIST  -> DbSchema.MovieDetails.TBL_NAME
+        else -> null
+    } ?: throw badUri(uri)
+
+    /**
+     * Helper function used to obtain the table name and selection arguments based on the
+     * given [uri]
+     * @param [uri] The received URI, which may refer to a table or to an individual entry
+     * @return A [Pair] instance bearing the table name (the Pair's first), the selection
+     * string (the Pair's second)
+     * @throws IllegalArgumentException if the received [uri] does not refer to a valid data set
+     */
+    private fun resolveTableAndSelectionInfoFromUri(uri: Uri, selection: String?): Pair<String, String?> {
+        val itemSelection = "${DbSchema.COL_ID} = ${uri!!.lastPathSegment}"
+        return when (uriMatcher.match(uri)) {
+            UPCOMING.ITEM -> Pair(DbSchema.UpcomingMovies.TBL_NAME, itemSelection)
+            THEATERS.ITEM -> Pair(DbSchema.NowPlayingMovies.TBL_NAME, itemSelection)
+            DETAILS.ITEM  -> Pair(DbSchema.MovieDetails.TBL_NAME, itemSelection)
+            else -> resolveTableInfoFromUri(uri).let { Pair(it, selection) }
+        }
+    }
+
+
+    override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>, sortOrder: String?): Cursor {
+
+        val params = resolveTableAndSelectionInfoFromUri(uri, selection)
+        val qbuilder = SQLiteQueryBuilder()
+        qbuilder.tables = params.first
+        qbuilder.appendWhere(params.second)
+        val db = dbHelper.readableDatabase
+        val cursor = qbuilder.query(db, projection, params.second, selectionArgs, null, null, sortOrder)
+        cursor.setNotificationUri(context.contentResolver, uri)
+        return cursor
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri {
+        val table = resolveTableAndSelectionInfoFromUri(uri, null).first
+        val db = dbHelper.writableDatabase
+        val newId = db.insert(table, null, values)
+
+        if (!inBatchMode.get())
+            context.contentResolver.notifyChange(uri, null)
+
+        return ContentUris.withAppendedId(uri, newId)
+
+    }
+
+    override fun update(uri: Uri?, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?): Int {
+        throw UnsupportedOperationException("not implemented")
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        val table = resolveTableAndSelectionInfoFromUri(uri, null).first
+        val db = dbHelper.writableDatabase
+
+        val ndel = db.delete(table, null, null)
+        if (ndel > 0 && !inBatchMode.get()) {
+            context.contentResolver.notifyChange(uri, null)
+        }
+        return ndel
+    }
+
+
+    val inBatchMode = object : ThreadLocal<Boolean>() {
+        override fun initialValue(): Boolean = false
+    }
+
+    override fun applyBatch(operations: ArrayList<ContentProviderOperation>?): Array<ContentProviderResult> {
+        val database = dbHelper.writableDatabase
+        inBatchMode.set(true)
+        database.beginTransaction()
+        val results : Array<ContentProviderResult>
+        try {
+            results = super.applyBatch(operations)
+            database.setTransactionSuccessful()
+        } finally {
+            database.endTransaction()
+            inBatchMode.set(false)
+        }
+        context.contentResolver.notifyChange(MovieContract.BASE_CONTENT_URI, null)
+        return results
+    }
 
     private fun badUri(uri: Uri?) =
             IllegalArgumentException("unknown uri: $uri")
-
-    override fun query(uri: Uri?, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor {        val qbuilder = SQLiteQueryBuilder()
-        var sortOrder = sortOrder
-        when (URI_MATCHER.match(uri)) {
-            UPCOMING.CURSOR -> {
-                qbuilder.tables = DbSchema.UpcomingMovies.TBL_NAME
-                if (TextUtils.isEmpty(sortOrder)) {
-                    sortOrder = MovieContract.UpcomingMovies.DEFAULT_SORT_ORDER
-                }
-            }
-            UPCOMING.ITEM -> {
-                qbuilder.tables = DbSchema.UpcomingMovies.TBL_NAME
-                qbuilder.appendWhere("${DbSchema.COL_ID} = ${uri!!.lastPathSegment}")
-            }
-            else -> throw badUri(uri)
-        }
-
-        val db = dbHelper.readableDatabase
-        val cursor = qbuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder)
-
-        cursor.setNotificationUri(context.contentResolver, uri)
-        
-        return cursor
-
-    }
-
-    override fun insert(p0: Uri?, p1: ContentValues?): Uri {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun update(p0: Uri?, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun delete(p0: Uri?, p1: String?, p2: Array<out String>?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 
 }
--- app/src/main/java/pdm_1718i/yamda/data/db/MoviesDbHelper.kt
+++ app/src/main/java/pdm_1718i/yamda/data/db/MoviesDbHelper.kt
@@ -7,13 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper
 class MoviesDbHelper(context: Context?) : SQLiteOpenHelper(context, DbSchema.DB_NAME, null, DbSchema.DB_VERSION){
 
         override fun onCreate(db: SQLiteDatabase?) {
-            db?.execSQL(DbSchema.MovieDetails.DDL_CREATE_TABLE)
-            db?.execSQL(DbSchema.NowPlayingMovies.DDL_CREATE_TABLE)
-            db?.execSQL(DbSchema.UpcomingMovies.DDL_CREATE_TABLE)
+            createDb(db)
         }
 
         override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-
-            TODO("migrate old db to new one")
+            deleteDb(db)
+            createDb(db)
+            return
+            TODO("refactor method so it migrate old db to new one")
         }
+
+    private fun createDb(db: SQLiteDatabase?) {
+        db?.execSQL(DbSchema.MovieDetails.DDL_CREATE_TABLE)
+        db?.execSQL(DbSchema.NowPlayingMovies.DDL_CREATE_TABLE)
+        db?.execSQL(DbSchema.UpcomingMovies.DDL_CREATE_TABLE)
     }
+
+    private fun deleteDb(db: SQLiteDatabase?) {
+        db?.execSQL(DbSchema.MovieDetails.DDL_DROP_TABLE)
+        db?.execSQL(DbSchema.NowPlayingMovies.DDL_DROP_TABLE)
+        db?.execSQL(DbSchema.UpcomingMovies.DDL_DROP_TABLE)
+    }
+}
