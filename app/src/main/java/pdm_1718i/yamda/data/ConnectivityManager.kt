aac7ade3bdc4485272e3c1a5270d2cfb02c4d488
jpedroduarte <redice_dragon@hotmail.com>
2017-11-29 17:13:11 +0000

    Added an implementation of ConnectivityManager.

---
+++ app/src/main/java/pdm_1718i/yamda/data/ConnectivityManager.kt
@@ -0,0 +1,26 @@
+package pdm_1718i.yamda.data
+
+import android.content.Context
+import android.net.ConnectivityManager
+import pdm_1718i.yamda.ui.App
+
+
+object ConnectivityManager{
+    val manager by lazy { App.instance.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
+
+    fun isConnected(): Boolean{
+        val networkInfo = manager.activeNetworkInfo
+        if(networkInfo != null && networkInfo.isConnectedOrConnecting){
+            return true
+        }
+        return false
+    }
+
+    fun isWifi(): Boolean{
+        val networkInfo = manager.activeNetworkInfo
+        if(networkInfo != null && networkInfo.type == ConnectivityManager.TYPE_WIFI){
+            return true
+        }
+        return false
+    }
+}
