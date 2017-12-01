cd720ef0fd53474bb531316a5a217ffbb2b04c46
0rph3u <david_mfra@hotmail.com>
2017-12-01 14:34:34 +0000

    initial implementation of provider access

---
+++ app/src/main/java/pdm_1718i/yamda/data/db/DbDataMapper.kt
@@ -0,0 +1,27 @@
+package pdm_1718i.yamda.data.db
+
+import android.database.Cursor
+import com.antonioleiva.weatherapp.domain.model.Forecast
+import com.antonioleiva.weatherapp.domain.model.ForecastList
+import pdm_1718i.yamda.model.Movie
+
+class DbDataMapper {
+
+    fun convertFromDomain(forecast: ForecastList) = with(forecast) {
+        val daily = dailyForecast.map { convertDayFromDomain(id, it) }
+        CityForecast(id, city, country, daily)
+    }
+
+    private fun convertDayFromDomain(cityId: Long, forecast: Forecast) = with(forecast) {
+        DayForecast(date, description, high, low, iconUrl, cityId)
+    }
+
+    fun upcomingMoviesToDomain(cursor: Cursor) : List<Movie>{
+
+
+    }
+
+    fun convertDayToDomain(dayForecast: DayForecast) = with(dayForecast) {
+        Forecast(_id, date, description, high, low, iconUrl)
+    }
+}
---
+++ app/src/main/java/pdm_1718i/yamda/data/db/MoviesDb.kt
@@ -0,0 +1,61 @@
+package pdm_1718i.yamda.data.db
+
+import android.graphics.Bitmap
+import android.widget.ImageView
+import pdm_1718i.yamda.data.MoviesDataSource
+import pdm_1718i.yamda.model.DetailedMovie
+import pdm_1718i.yamda.model.Movie
+import pdm_1718i.yamda.ui.App
+
+class MoviesDb (private val provider : MoviesDataSource): MoviesDataSource{
+
+    override fun popularMovies(page: Int, completionHandler: (movies: List<Movie>) -> Unit){
+        provider.popularMovies(page,completionHandler)
+    }
+
+    override fun upcomingMovies(page: Int, completionHandler: (movies: List<Movie>) -> Unit){
+        val movieCursor = App.instance.contentResolver.query(MovieContract.UpcomingMovies.CONTENT_URI, null, null, null, null)
+
+        if (movieCursor.count > 0) {
+            movieCursor.moveToFirst()
+            val movies : MutableList<Movie>
+            while(!movieCursor.isAfterLast())
+                movies.add(Movie(movieCursor.getString(MovieContract.MovieDetails)))
+
+                //Gather values
+                String id = c . getString (1);
+                String name = c . getString (iname);
+                String isbn = c . getString (iisbn);
+
+            }
+                movieCursor.moveToNext()
+        }
+    }
+
+    override fun playingMovies (page: Int, completionHandler: (movies: List<Movie>) -> Unit){
+
+
+    }
+
+    override fun movieSearch(query: String, page : Int, completionHandler: (movies: List<Movie>) -> Unit){
+
+
+    }
+
+    override fun movieDetail(id: Int, completionHandler: (movies: DetailedMovie) -> Unit){
+
+    }
+
+    override fun movieImage(image_id: String, imageView: ImageView, image_size: String){
+
+
+    }
+
+    override fun movieImage(image_id: String, bitmapCompletionHandler: (bitmap: Bitmap)-> Unit, image_size: String){
+
+
+
+    }
+
+
+}
