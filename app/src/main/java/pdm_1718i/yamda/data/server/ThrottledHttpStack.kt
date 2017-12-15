8a07cb4cadd66c0af99be0316a55ad3e456a8ae3
0rph3u <david_mfra@hotmail.com>
2017-12-15 23:38:48 +0000

    fixed Throttle dispatcher, only throttles the api that has a request limit

--- app/src/main/java/pdm_1718i/yamda/data/server/ThrottledHttpStack.kt
+++ app/src/main/java/pdm_1718i/yamda/data/server/ThrottledHttpStack.kt
@@ -2,25 +2,33 @@ package pdm_1718i.yamda.data.server
 
 import android.net.http.AndroidHttpClient
 import android.os.Build
+import android.util.Log
 import com.android.volley.Request
 import com.android.volley.toolbox.HttpClientStack
 import com.android.volley.toolbox.HttpStack
 import com.android.volley.toolbox.HurlStack
+import org.apache.http.conn.ConnectTimeoutException
 import java.net.URL
 import java.util.concurrent.ConcurrentHashMap
 
+class ThrottlePolicy(val authority : String, val retries :Int, val maxRequestsInTimeFrame:Int, val timeFrame: Int)
 
-class ThrottlePolicy(val authorities : List<String>, val retries :Int, val maxRequestsinTimeFrame :Int, val timeFrame: Int)
 
+
+/**
+ *Wrapper over [HttpStack] that blocks requesting thread
+ *  if the request is going to exceed the limit of requests the authority have on a time frame
+ *  thread is only blocked the remaining time until it can proceed with the request
+ */
 class ThrottledHttpStack(policies :List<ThrottlePolicy>) : HttpStack{
 
-        private val httpStack = if (Build.VERSION.SDK_INT >= 9) { HurlStack() } else { HttpClientStack(AndroidHttpClient.newInstance("Volley/0")) }
-        private val dispatchers : ConcurrentHashMap<String, ThrottledDispatcher> = ConcurrentHashMap()
+        private val httpStack = if (Build.VERSION.SDK_INT >= 9) { HurlStack() } else { HttpClientStack(AndroidHttpClient.newInstance("Volley/0")) } //taken from Volley.newRequestQueue source code so it's consistent
+        private val dispatchers : ConcurrentHashMap<String, ThrottledDispatcher> = ConcurrentHashMap() // map's authority to corresponding dispatcher
 
         init {
         policies.forEach {
             val dispatcher = ThrottledDispatcher(it, httpStack)
-            it.authorities.forEach {dispatchers.put(it, dispatcher)}
+            dispatchers.put(it.authority, dispatcher)
         }
 
     }
@@ -28,12 +36,10 @@ class ThrottledHttpStack(policies :List<ThrottlePolicy>) : HttpStack{
         val authority : String = URL(request?.url).authority
 
         return if(dispatchers.containsKey(authority)){
-            dispatchers[authority]?.put(DispatchableRequest(request,additionalHeaders))?.get()!!
+            try { dispatchers[authority]?.put(DispatchRequest(request,additionalHeaders))?.get()!! } catch (e: Exception){ Log.e("ThrottledHttpStack", e.message); throw e}
+
         }else{
             httpStack.performRequest(request, additionalHeaders)
         }
-
     }
-
-
 }
