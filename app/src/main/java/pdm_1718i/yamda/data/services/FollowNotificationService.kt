fe6a5f5a9a64224bd0c38a3040454ae75d85f025
jpedroduarte <redice_dragon@hotmail.com>
2017-12-17 18:26:12 +0000

    Finished implementing first version of FollowNotificationService.

--- app/src/main/java/pdm_1718i/yamda/data/services/FollowNotificationService.kt
+++ app/src/main/java/pdm_1718i/yamda/data/services/FollowNotificationService.kt
@@ -9,7 +9,6 @@ import android.os.Build
 import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
 import pdm_1718i.yamda.R
-import pdm_1718i.yamda.data.server.Options
 import pdm_1718i.yamda.extensions.toast
 import pdm_1718i.yamda.ui.App
 
@@ -21,37 +20,34 @@ class FollowNotificationService: JobService(){
 
     override fun onStartJob(jobParameters: JobParameters?): Boolean {
         toast("OnStartJob - Follow Service")
+        val movie_id: Int = jobParameters?.extras?.getInt(JobNotification.BUNDLE_ID) ?: return false
+
         async {
-            val movieJob = jobParameters?.extras?.getInt(JobNotification.BUNDLE_ID)?.let {
-                bg {try {App.moviesProvider.movieDetail(it)}catch (e: Throwable){null}}
+            val movieJob = bg {
+                try {
+                    App.moviesProvider.movieDetail(movie_id)
+                } catch (e: Throwable) {
+                    null
+                }
             }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 //todo create Notification Channel
                 Notification.Builder(this@FollowNotificationService, "")
-            }else{
+            } else {
                 Notification.Builder(this@FollowNotificationService)
             }.apply {
-                movieJob?.await()?.run {
-                    setContentText(this.title)
-                    if (poster_path != null && poster_path.isNotEmpty()) {
-                        try{App.moviesProvider.image(poster_path, Options.poster_sizes[Options.SMALL]!!, {setLargeIcon(it)}) }catch (e:Throwable){null}
-                    }
-                }
-
                 setContentTitle("New movie released")
                 setSmallIcon(R.drawable.ic_video_camera_80s)
-                //setLargeIcon(BitmapFactory.decodeResource(App.instance.resources, R.drawable.ic_video_camera))
-
-                //todo set movie image as icon
-                //setContentIntent(pIntent)
                 setAutoCancel(false)
+                movieJob?.await()?.run {
+                    setContentText(this.title)
+                }
 
                 val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notificationManager.notify(123, build())
+                notificationManager.notify(movie_id, build())
             }
         }
-
         return true //code is running asynchronously
     }
 
