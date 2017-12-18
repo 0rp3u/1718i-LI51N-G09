1ab1e899b3b25d9ceea1c202be0ac906ea393a5c
jpedroduarte <redice_dragon@hotmail.com>
2017-12-18 16:58:59 +0000

    Updated DBSyncJob to support instantaneous scheduling by sending no parameters to method schedule.

--- app/src/main/java/pdm_1718i/yamda/data/services/DBSyncJob.kt
+++ app/src/main/java/pdm_1718i/yamda/data/services/DBSyncJob.kt
@@ -4,7 +4,9 @@ import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.content.ComponentName
 import android.content.Context
+import android.os.Build
 import android.os.PersistableBundle
+import pdm_1718i.yamda.data.utils.UtilPreferences
 import pdm_1718i.yamda.extensions.getNetworkTypeFromPreferences
 import pdm_1718i.yamda.ui.App
 import java.util.*
@@ -13,21 +15,23 @@ object DBSyncJob{
     val BUNDLE_ID = "id"
     val JOB_ID = -1
 
-    private fun newInstance(calendar: Calendar): JobInfo {
+    private fun newInstance(calendar: Calendar?): JobInfo {
         return JobInfo.Builder(
                 JOB_ID,
                 ComponentName(App.instance.applicationContext, DatabaseUpdater::class.java)
         ).run {
-            //todo meter depois do edgar fazer a preference
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                setRequiresBatteryNotLow(true)  //check preferences
-//            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                setRequiresBatteryNotLow(!UtilPreferences.getLowBatteryUpdate())
+            }
 
             setRequiredNetworkType(this.getNetworkTypeFromPreferences())
             setPersisted(true)
-            //setMinimumLatency(calendar.FromPresentInMillis()) //todo change setMinimumLatency on release
-            setMinimumLatency(1000 * 5) //wait at least
-            
+
+            if(calendar != null){
+                //setMinimumLatency(calendar.FromPresentInMillis()) //todo change setMinimumLatency on release
+                setMinimumLatency(1000 * 5)
+            }
+
             setExtras(PersistableBundle().apply {
                 putInt(BUNDLE_ID, JOB_ID)
             })
@@ -35,7 +39,7 @@ object DBSyncJob{
         }
     }
 
-    fun schedule(id: Int, calendar: Calendar): Boolean{
+    fun schedule(calendar: Calendar? = null): Boolean{
         val jobScheduler = App.instance.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
         return when(jobScheduler.schedule(newInstance(calendar))){
             JobScheduler.RESULT_SUCCESS -> true
